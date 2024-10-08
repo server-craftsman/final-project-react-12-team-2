918b3139ee5f562a3c586f8423d1dc340f155495
Vuong-king <vuong9408@gmail.com>
2024-10-08 14:38:45 +0700

    sua dashboard

---
+++ src/pages/admin/DashBoardAdmin.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import DashBoard from '../../components/admin/DashBoard'
+
+const DashBoardAdmin = () => {
+  return (
+    <div>
+      <DashBoard/>
+    </div>
+  )
+}
+
+export default DashBoardAdmin
--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,9 +1,9 @@
 import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
+import DashBoardAdmin from '../pages/admin/DashBoardAdmin'
 const ViewUserProfileDetail = lazy(() => import('../components/admin/ViewUserProfileDetail'))
 const Profile = lazy(() => import('../pages/admin/Profile'))
 const ManageUser = lazy(() => import('../pages/admin/ManageUser'))
-const Dashboard = lazy(() => import('../components/admin/DashBoard'))
 const AdminLayout = lazy(() => import('../layout/admin/AdminLayout'))
 
 const adminRoutes: RouteObject[] = [
@@ -11,7 +11,7 @@ const adminRoutes: RouteObject[] = [
     path: '/admin',
     element: <AdminLayout />,
     children: [
-      { index: true, element: <Dashboard /> },
+      { index: true, element: <DashBoardAdmin /> },
       { path: 'admin-info', element: <Profile /> },
       { path: 'manage-user', element: <ManageUser /> },
       { path: 'view-user/:id', element: <ViewUserProfileDetail /> },
