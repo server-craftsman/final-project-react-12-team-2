1fec02efab43df3ec7cb8a62ca8e242e454b6229
server-craftsman <huyit2003@gmail.com>
2024-10-10 00:34:57 +0700

    setup env

--- src/const/authentication.tsx
+++ src/const/authentication.tsx
@@ -1,2 +1,2 @@
-export const CLIENT_ID = '423226612557-dcadbtt5rnfi7u2mhlpmsmm08uift3l2.apps.googleusercontent.com';
-export const REDIRECT_URI = 'http://localhost:3000/';
+export const CLIENT_ID = import.meta.env.REACT_APP_GOOGLE_CLIENT_ID || '423226612557-dcadbtt5rnfi7u2mhlpmsmm08uift3l2.apps.googleusercontent.com';
+export const REDIRECT_URI = import.meta.env.REACT_APP_REDIRECT_URI || 'http://localhost:3000/';
