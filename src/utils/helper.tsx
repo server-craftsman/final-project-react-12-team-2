3b822e311e6059601abd9c3806bc9ee7a98ac46a
server-craftsman <huyit2003@gmail.com>
2024-10-09 15:56:38 +0700

    fix utils

--- src/utils/helper.tsx
+++ src/utils/helper.tsx
@@ -11,7 +11,7 @@ export const moneyFormat = (money: number) => {
     return money.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' }).replace(/\./g, ',');
 };
 
-export function getOrderStatus(status) {
+export function getOrderStatus(status: string) {
     // Normalize the status to uppercase for comparison
     const normalizedStatus = status?.toUpperCase();
   
@@ -20,20 +20,20 @@ export function getOrderStatus(status) {
       case "PUBLISHED":
         return (
           <span className="capitalize py-1 px-2 rounded-md text-xs text-blue-600 bg-blue-100">
-            {normalizedStatus.replaceAll("_", " ").toLowerCase()}
+            {normalizedStatus.replace("_", " ").toLowerCase()}
           </span>
         );
       case "DRAFT":
         return (
           <span className="capitalize py-1 px-2 rounded-md text-xs text-orange-600 bg-orange-100">
-            {normalizedStatus.replaceAll("_", " ").toLowerCase()}
+            {normalizedStatus.replace("_", " ").toLowerCase()}
           </span>
         );
   
       default:
         return (
           <span className="capitalize py-1 px-2 rounded-md text-xs text-gray-600 bg-gray-100">
-            {normalizedStatus?.replaceAll("_", " ").toLowerCase() ||
+            {normalizedStatus?.replace("_", " ").toLowerCase() ||
               "unknown status"}
           </span>
         );
