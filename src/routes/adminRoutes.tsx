71688f33d69dd921dc32f49b9780e805777a0e54
hai11062003 <truonggiahai007@gmail.com>
2024-10-09 08:26:38 +0700

    add category management

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,24 +1,39 @@
-import { lazy } from 'react'
-import { RouteObject } from 'react-router-dom'
-import DashBoardAdmin from '../pages/admin/DashBoardAdmin'
-const ViewUserProfileDetail = lazy(() => import('../components/admin/ViewUserProfileDetail'))
-const Profile = lazy(() => import('../pages/admin/Profile'))
-const ManageUser = lazy(() => import('../pages/admin/ManageUser'))
-const AdminLayout = lazy(() => import('../layout/admin/AdminLayout'))
-const EditUserProfile = lazy(() => import('../components/admin/EditUserProfile'))
+import { lazy } from "react";
+import { RouteObject } from "react-router-dom";
+import DashBoardAdmin from "../pages/admin/DashBoardAdmin";
+import ManageCategory from "../pages/admin/ManageCategory";
+import DetailsCategory from "../components/admin/DetailsCategoty";
+const ViewUserProfileDetail = lazy(
+  () => import("../components/admin/ViewUserProfileDetail")
+);
+const Profile = lazy(() => import("../pages/admin/Profile"));
+const ManageUser = lazy(() => import("../pages/admin/ManageUser"));
+const AdminLayout = lazy(() => import("../layout/admin/AdminLayout"));
+const EditUserProfile = lazy(
+  () => import("../components/admin/EditUserProfile")
+);
 
 const adminRoutes: RouteObject[] = [
   {
-    path: '/admin',
+    path: "/admin",
     element: <AdminLayout />,
     children: [
       { index: true, element: <DashBoardAdmin /> },
-      { path: 'admin-info', element: <Profile /> },
-      { path: 'manage-user', element: <ManageUser /> },
-      { path: 'view-user/:id', element: <ViewUserProfileDetail /> },
-      { path: 'edit-user/:id', element: <EditUserProfile /> },
+      { path: "admin-info", element: <Profile /> },
+      { path: "manage-user", element: <ManageUser /> },
+      { path: "view-user/:id", element: <ViewUserProfileDetail /> },
+      { path: "edit-user/:id", element: <EditUserProfile /> },
+      { path: "categories", element: <ManageCategory /> },
+      {
+        path: "categories/details-category",
+        element: <DetailsCategory />,
+      },
+      {
+        path: "categories/categories-details/:id",
+        element: <DetailsCategory />,
+      },
     ],
   },
-]
+];
 
-export default adminRoutes
+export default adminRoutes;
