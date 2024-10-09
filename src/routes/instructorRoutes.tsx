859db895c5f07ed95bc90d1d8d7870c12ab730f5
Sang Truong <nhoksang20022003@gmail.com>
2024-10-09 10:06:08 +0700

    add instructorchangepassword and improve setting instructor

--- src/routes/instructorRoutes.tsx
+++ src/routes/instructorRoutes.tsx
@@ -2,8 +2,6 @@ import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
 const InstructorLayout = lazy(() => import('../layout/instructor/InstructorLayout'))
 const Dashboard = lazy(() => import('../components/instructor/Dashboard'))
-const InstructorProfile = lazy(() => import('../components/instructor/profile/InstructorProfile'))
-//const InstructorChangePassword = lazy(() => import('../components/instructor/profile/InstructorChangePassword'))
 const Setting = lazy(() => import('../components/instructor/Setting'))
 
 const instructorRoutes: RouteObject[] = [
@@ -13,8 +11,7 @@ const instructorRoutes: RouteObject[] = [
         children: [
             { index: true, element: <Dashboard /> },
             { path: 'setting', element: <Setting /> },
-            { path: 'profile', element: <InstructorProfile /> },
-            //{ path: 'change-password', element: <InstructorChangePassword /> },
+
 
         ],
     },
