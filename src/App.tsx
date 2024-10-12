fb451d3980a89720477b180ebcd458ec8c3560d8
server-craftsman <huyit2003@gmail.com>
2024-10-12 19:22:03 +0700

    delete subs route in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,9 @@ import Loading from './components/generic/home/Loading'
 import ScrollToTopButton from './components/generic/home/ScrollToTopButton'
 
 //Import Routes
-// import authRoutes from './routes/authRoutes'
-// import studentRoutes from './routes/studentRoutes'
-// import adminRoutes from './routes/adminRoutes'
-// import instructorRoutes from './routes/instructorRoutes'
-// import commonRoutes from './routes/commonRoutes'
 import protectedRoutes from './routes/protected/protectedRoutes'
 import publishRoutes from './routes/publish/publishRoutes'
-import { CartProvider } from './context/CartContext'; // Add this import
+import { CartProvider } from './context/CartContext';
 //==============================
 
 const App = () => {
