9731b8eb59cc01ffbaf7df4548fd51de782c8276
server-craftsman <huyit2003@gmail.com>
2024-10-12 18:31:30 +0700

    divide Common Routes in const folder

---
+++ src/const/constCommon.tsx
@@ -0,0 +1,17 @@
+import { lazy } from 'react';
+
+//lazy import
+export const MainLayout = lazy(() => import('../layout/main-layout/MainLayout'));
+export const HomePage = lazy(() => import('../pages/home/HomePage'));
+export const CourseDetails = lazy(() => import('../components/generic/courses/main-display/CourseDetails'));
+export const LessonDetails = lazy(() => import('../components/generic/lessons/LessonDetails'));
+export const SessionDetails = lazy(() => import('../components/generic/sessions/SessionDetails'));
+export const CartPage = lazy(() => import('../components/generic/cart/CartPage'));
+
+//routes
+export const homeRoute = { index: true, element: <HomePage /> };
+export const courseDetailsRoute = { path: '/course/:id', element: <CourseDetails /> };
+export const courseByIdRoute = { path: '/course/:courseId', element: <CourseDetails /> };
+export const sessionDetailsRoute = { path: '/course/:courseId/session/:sessionId', element: <SessionDetails /> };
+export const lessonDetailsRoute = { path: '/course/:courseId/lesson/:lessonId', element: <LessonDetails /> };
+export const cartRoute = { path: "cart", element: <CartPage/> };
--- src/routes/commonRoutes.tsx
+++ src/routes/commonRoutes.tsx
@@ -1,27 +1,27 @@
-import { lazy } from 'react'
 import { RouteObject } from 'react-router-dom'
-
-//import lazy
-const MainLayout = lazy(() => import('../layout/main-layout/MainLayout'))
-const HomePage = lazy(() => import('../pages/home/HomePage'))
-const CourseDetails = lazy(() => import('../components/generic/courses/main-display/CourseDetails'))
-const LessonDetails = lazy(() => import('../components/generic/lessons/LessonDetails'))
-const SessionDetails = lazy(() => import('../components/generic/sessions/SessionDetails'))
-const CartPage = lazy(() => import('../components/generic/cart/CartPage'))
-//==============================================================
+import {
+  MainLayout,
+  homeRoute,
+  courseDetailsRoute,
+  courseByIdRoute,
+  sessionDetailsRoute,
+  lessonDetailsRoute,
+  cartRoute
+} from '../const/constCommon'
 
 const commonRoutes: RouteObject[] = [
   {
     path: '/',
     element: <MainLayout />,
     children: [
-      { index: true, element: <HomePage /> },
-      { path: '/course/:id', element: <CourseDetails /> },
-      { path: '/course/:courseId', element: <CourseDetails /> },
-      { path: '/course/:courseId/session/:sessionId', element: <SessionDetails /> },
-      { path: '/course/:courseId/lesson/:lessonId', element: <LessonDetails /> },
-      {path: "cart", element: <CartPage/>}
+      homeRoute,
+      courseDetailsRoute,
+      courseByIdRoute,
+      sessionDetailsRoute,
+      lessonDetailsRoute,
+      cartRoute
     ],
   }
 ]
+
 export default commonRoutes
