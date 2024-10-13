e85df991941a18a53479d21d0ba2b0bbfde52567
server-craftsman <huyit2003@gmail.com>
2024-10-13 23:11:15 +0700

    custom build notification

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,52 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import notifier from 'node-notifier'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'custom-build-log',
+      buildStart() {
+        const startTime = new Date().toLocaleTimeString();
+        console.log(`🚀 Build starting at ${startTime}...`);
+        notifier.notify({
+          title: 'Vite Build',
+          message: `🚀 Build starting at ${startTime}...`,
+          sound: true,
+          wait: true, // Wait for user action before continuing
+        });
+      },
+      buildEnd() {
+        const endTime = new Date().toLocaleTimeString();
+        console.log(`🎉 Build finished at ${endTime}!`);
+        notifier.notify({
+          title: 'Vite Build',
+          message: `🎉 Build finished at ${endTime}!`,
+          sound: true,
+          wait: true, // Wait for user action before continuing
+        });
+      }
+    }
+  ],
   server: {
     port: 3000,
+    open: true,
   },
   resolve: {
     alias: {
-      '@src': path.resolve(__dirname, './src')
+      '@src': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@context': path.resolve(__dirname, './src/context'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@routes': path.resolve(__dirname, './src/routes'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@models': path.resolve(__dirname, './src/models'),
+      '@services': path.resolve(__dirname, './src/services'),
     }
   },
   build: {
