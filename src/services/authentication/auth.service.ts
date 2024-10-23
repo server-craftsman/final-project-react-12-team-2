7e55099420546d779c43d96ba0e296b7347c9534
server-craftsman <huyit2003@gmail.com>
2024-10-23 09:11:23 +0700

    remove toggle Loading from auth service api

--- src/services/authentication/auth.service.ts
+++ src/services/authentication/auth.service.ts
@@ -3,16 +3,11 @@ import { User, UserRole } from "../../models/User";
 import { API } from "../../const/api.path";
 
 export const AuthService = {
-  login(params: {
-    email: string;
-    password: string;
-    toggleLoading: (isLoading: boolean) => void;
-  }) {
+  login(params: { email: string; password: string }) {
     return BaseService.post<{ user: User; role: UserRole; token: string }>({
       url: API.AUTH.LOGIN,
       payload: params,
       isLoading: true,
-      toggleLoading: params.toggleLoading,
     }).then((response) => {
       const { token } = response.data;
       localStorage.setItem("authToken", token); // Store token in local storage
