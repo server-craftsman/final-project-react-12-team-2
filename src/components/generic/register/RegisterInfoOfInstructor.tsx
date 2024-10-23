a2bcc866479ddf2b76e1c0557a2af5320ac0faba
kaitou141 <hdk18112003@gmail.com>
2024-10-23 14:01:32 +0700

    add create button student and instructor

---
+++ src/components/generic/register/RegisterInfoOfInstructor.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Form, Input, Upload, Button } from 'antd'
+import { UploadOutlined, PhoneOutlined } from '@ant-design/icons'
+
+const RegisterInfoOfInstructor: React.FC = () => {
+  return (
+    <Form layout="vertical" className="mt-4">
+      <Form.Item
+        name="phone"
+        rules={[{ required: true, message: "Please input your phone number!" }]}
+      >
+        <Input
+          prefix={<PhoneOutlined className="site-form-item-icon text-indigo-600"/>}
+          placeholder="Phone Number"
+        />
+      </Form.Item>
+
+      <Form.Item
+        name="description"
+        rules={[{ required: true, message: "Please input a description!" }]}
+      >
+        <Input.TextArea placeholder="Description" />
+      </Form.Item>
+      <div className="flex justify-between">
+        <Form.Item
+          name="video"
+          label="Video"
+          rules={[{ required: true, message: "Please upload an introduction video!" }]}
+        >
+          <Upload>
+            <Button icon={<UploadOutlined className="site-form-item-icon text-indigo-600"/>} className="rounded-full w-24 h-24">Upload</Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item
+          name="avatar"
+          label="Avatar"
+          rules={[{ required: true, message: "Please upload an avatar!" }]}
+        >
+          <Upload>
+            <Button icon={<UploadOutlined className="site-form-item-icon text-indigo-600"/>} className="rounded-full w-24 h-24">Upload</Button>
+          </Upload>
+        </Form.Item>
+      </div>
+    </Form>
+  );
+};
+
+export default RegisterInfoOfInstructor;
