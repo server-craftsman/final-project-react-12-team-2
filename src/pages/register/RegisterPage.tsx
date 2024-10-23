6af682dd661616645972fabb26aec41c457909eb
kaitou141 <hdk18112003@gmail.com>
2024-10-23 14:02:38 +0700

    fix error

--- src/pages/register/RegisterPage.tsx
+++ src/pages/register/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import { Link } from "react-router-dom";
 import { Form, Input, Button, Typography, Divider } from "antd";
 import {
