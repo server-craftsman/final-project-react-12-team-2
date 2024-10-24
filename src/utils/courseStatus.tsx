e66cc693a0127de0fde7367c25e0fe00db2950fd
Vuong-king <vuong9408@gmail.com>
2024-10-24 13:44:26 +0700

    edit status

--- src/utils/courseStatus.tsx
+++ src/utils/courseStatus.tsx
@@ -1,18 +1,35 @@
 import { CourseStatusEnum } from "../models/Course";
 
+
 export const courseStatusColor = (status: CourseStatusEnum) => {
+  let classNames = "";
+
   switch (status) {
     case "waiting_approve":
-      return "bg-yellow-100 text-yellow-700 border border-yellow-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-yellow-100 text-yellow-700 border border-yellow-500";
+      break;
     case "approve":
-      return "bg-green-100 text-green-700 border border-green-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-green-100 text-green-700 border border-green-500";
+      break;
     case "reject":
-      return "bg-red-100 text-red-700 border border-red-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-red-100 text-red-700 border border-red-500";
+      break;
     case "active":
-      return "bg-blue-100 text-blue-700 border border-blue-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-blue-100 text-blue-700 border border-blue-500";
+      break;
     case "inactive":
-      return "bg-gray-100 text-gray-700 border border-gray-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-gray-100 text-gray-700 border border-gray-500";
+      break;
     case "new":
-      return "bg-purple-100 text-purple-700 border border-purple-500 rounded-md px-2 py-1 text-xs capitalize";
+      classNames = "bg-purple-100 text-purple-700 border border-purple-500";
+      break;
+    default:
+      classNames = "";
   }
+
+  return (
+    <span className={`${classNames} rounded-md px-2 py-1 text-xs capitalize`}>
+      {status}
+    </span>
+  );
 };
