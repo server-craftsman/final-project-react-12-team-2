5bdf96f9949728e47c558354a0850308c7510d46
Sang Truong <nhoksang20022003@gmail.com>
2024-11-07 20:43:14 +0700

    get user detail subscriber

--- src/models/api/request/subscription/update.request.model.ts
+++ src/models/api/request/subscription/update.request.model.ts
@@ -1,3 +1,13 @@
 export interface UpdateSubscriptionsParams {
   instructor_id: string;
 }
+export interface GetSubscriptionsParams {
+  searchCondition: {
+    keyword?: string;
+    is_delete?: boolean | undefined;
+  };
+  pageInfo: {
+    pageNum?: number;
+    pageSize?: number;
+  };
+}
--- src/pages/profile/ProfileSubscribeManagement.tsx
+++ src/pages/profile/ProfileSubscribeManagement.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ProfileSubscribe from '../../components/generic/profile/ProfileSubscribe'
-import { useParams } from 'react-router-dom';
+
 //debug but still not working - oke lol Sang
 const ProfileSubscribeManagement: React.FC = () => {
-    const instructorId = useParams().instructorId;
+
     return (
-        <ProfileSubscribe instructorId={instructorId as string} />
+        <ProfileSubscribe />
     )
 }
 
