8830005f79b79f9ff778140b40d4110529b1097d
Nguyễn Đan Huy <huyit2003@gmail.com>
2024-11-14 13:31:41 +0700

    add logic about display cart if local has userInfo

--- src/layout/main-layout/MainNavbar.tsx
+++ src/layout/main-layout/MainNavbar.tsx
@@ -110,12 +110,14 @@ const Navbar = () => {
               <input type="text" placeholder="Search..." className="w-40 rounded-full bg-[#161d66] px-4 py-2 pl-10 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-[#161d66] lg:w-64" />
               <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 transform text-gray-400" />
             </div>
-            <Link to="/cart" className="relative hidden text-gray-300 hover:text-white lg:block">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-              </svg>
-              {cartItems.length > 0 && <span className="absolute right-0 top-0 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">{cartItems.length}</span>}
-            </Link>
+            {userInfo && (
+              <Link to="/cart" className="relative hidden text-gray-300 hover:text-white lg:block">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                </svg>
+                {cartItems.length > 0 && <span className="absolute right-0 top-0 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs text-white">{cartItems.length}</span>}
+              </Link>
+            )}
             {userInfo ? (
               <div className="group relative">
                 <div className="flex items-center space-x-4">
