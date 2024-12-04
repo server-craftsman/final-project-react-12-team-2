839ae7cc7eb0497d850d97cb36cea5ee70b00360
server-craftsman <huyit2003@gmail.com>
2024-12-04 11:18:36 +0700

    [NguyenDanHuy_04.12.2024] Enhance Navbar component to include token validation for user actions. Updated cart count logic to check for userInfo and token before fetching new cart count. Adjusted rendering of cart button to require both userInfo and token for display.

--- src/layout/main-layout/MainNavbar.tsx
+++ src/layout/main-layout/MainNavbar.tsx
@@ -14,7 +14,7 @@ import { RootState } from "../../app/redux/store";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { getNewCartCount, updateCartItems } = useCart();
-  const { userInfo, logout } = useAuth();
+  const { userInfo, logout, token } = useAuth();
 
   const [searchKeyword, setSearchKeyword] = useState("");
   const navigate = useNavigate();
@@ -37,12 +37,13 @@ const Navbar = () => {
 
   useEffect(() => {
     const updateCartCount = async () => {
+      if (!userInfo || !token) return;
       const count = await getNewCartCount();
       dispatch(setCartCount(count));
     };
 
     updateCartCount();
-  }, [getNewCartCount, dispatch]);
+  }, [getNewCartCount, dispatch, userInfo, token]);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -158,7 +159,7 @@ const Navbar = () => {
                 </AutoComplete>
               </form>
             </div>
-            {userInfo && (
+            {userInfo && token && (
               <button onClick={handleDisplayCart} className="relative flex items-center justify-center p-2 bg-indigo-600 rounded-full hover:bg-indigo-700 transition duration-300">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6 text-white">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
