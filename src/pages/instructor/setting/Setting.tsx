871695c82ff72ab8c714df33767eaf8adf2084f1
server-craftsman <huyit2003@gmail.com>
2024-12-04 13:13:01 +0700

    [NguyenDanHuy_04.12.2024] Refactor Setting components for instructors and students to conditionally display the "Change Password" tab based on the presence of google_id in userInfo. Improved code readability by restructuring the items array and ensuring consistent handling of user information retrieval.

--- src/pages/instructor/setting/Setting.tsx
+++ src/pages/instructor/setting/Setting.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import { Tabs } from "antd";
 import InstructorInfo from "../../../components/instructor/setting/InstructorInfo";
 import ChangePasswordInstructor from "../../../components/instructor/setting/ChangePasswordInstructor";
+import { storage } from "../../../utils";
 
 const Setting: React.FC = () => {
+  const userInfo = JSON.parse(storage.getItemInLocalStorage("userInfo") || "{}");
+  const googleId = userInfo?.google_id;
   const items = [
     {
       label: "Instructor Info",
       key: "1",
       children: <InstructorInfo />
-    },
-    {
+    }
+  ];
+
+  if (!googleId) {
+    items.push({
       label: "Change Password",
       key: "2",
       children: <ChangePasswordInstructor />
-    }
-  ];
+    });
+  }
 
   return <Tabs defaultActiveKey="1" items={items} />;
 };
--- src/pages/student/setting/Setting.tsx
+++ src/pages/student/setting/Setting.tsx
@@ -1,20 +1,26 @@
 import StudentInformation from "../../../components/student/setting/StudentInformation";
 import ChangePassword from "../../../components/student/setting/ChangePassword";
 import { Tabs } from "antd";
+import { storage } from "../../../utils";
 
-const Setting = () => {
+const Setting: React.FC = () => {
+  const userInfo = JSON.parse(storage.getItemInLocalStorage("userInfo") || "{}");
+  const googleId = userInfo?.google_id;
   const items = [
     {
       label: "Information",
       key: "1",
       children: <StudentInformation />
     },
-    {
+  ];
+
+  if (!googleId) {
+    items.push({
       label: "Change Password",
       key: "2",
       children: <ChangePassword visible={true} currentPassword="" />
-    }
-  ];
+    });
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 p-5 md:p-10">
